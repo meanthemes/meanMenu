40ec24270b4cad633c834bc43f1aff7e68694197
mashalla <mashalla@ubuntu.(none)>
2013-02-27 13:49:49 -0600

    Larger menu icon, trigger at 1000px

--- jquery.meanmenu.js
+++ jquery.meanmenu.js
@@ -27,13 +27,13 @@
         var defaults = {
             meanMenuTarget: jQuery(this), // Target the current HTML markup you wish to replace
             meanMenuClose: "X", // single character you want to represent the close menu button
-            meanMenuCloseSize: "18px", // set font size of close button
+            meanMenuCloseSize: "2em", // set font size of close button
             meanMenuOpen: "<span /><span /><span />", // text/markup you want when menu is closed
             meanRevealPosition: "right", // left right or center positions
             meanRevealPositionDistance: "0", // Tweak the position of the menu
             meanRevealColour: "", // override CSS colours for the reveal background
             meanRevealHoverColour: "", // override CSS colours for the reveal hover
-            meanScreenWidth: "480", // set the screen width you want meanmenu to kick in at
+            meanScreenWidth: "1000", // set the screen width you want meanmenu to kick in at
             meanNavPush: "", // set a height here in px, em or % if you want to budge your layout now the navigation is missing.
             meanShowChildren: true, // true to show children in the menu, false to hide them
             meanRemoveAttrs: false // true to remove classes and IDs, false to keep them
